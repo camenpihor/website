b5f0b70c7c5aa7fb7951a199535b91718c22d383
Camen <camenpihor@gmail.com>
2020-05-14 15:06:58 -0500

    Use routejson for home site map

--- frontend/src/routes/Home.vue
+++ frontend/src/routes/Home.vue
@@ -47,48 +47,32 @@
           <div class="column">
             <h1 class="home__site-map__header">General</h1>
             <ul class="home__site-map__list">
-              <li class="home__site-map__item">
-                <router-link :to="{ name: 'home' }">Home</router-link>
-              </li>
-              <li class="home__site-map__item">
-                <router-link :to="{ name: 'about' }">About</router-link>
-              </li>
-              <li class="home__site-map__item">
-                <router-link :to="{ name: 'recommendations' }"
-                  >Recommendations</router-link
+              <li
+                class="home__site-map__item"
+                v-for="route in routeJson.general"
+                :key="route.label"
+              >
+                <router-link
+                  class="home__site-map__item__label"
+                  :to="{ name: route.name }"
+                  >{{ route.label }}</router-link
                 >
-              </li>
-              <li class="home__site-map__item">
-                <router-link :to="{ name: 'github' }">GitHub</router-link>
               </li>
             </ul>
           </div>
           <div class="column">
             <h1 class="home__site-map__header">Projects</h1>
-            <ul class="home__site-map__group">
-              <li class="home__site-map__item">
-                <router-link :to="{ name: 'error' }">Blog</router-link>
-              </li>
-              <li class="home__site-map__item">
-                <router-link :to="{ name: 'error' }"
-                  >Math Implementations</router-link
+            <ul class="home__site-map__list">
+              <li
+                class="home__site-map__item"
+                v-for="route in routeJson.projects"
+                :key="route.label"
+              >
+                <router-link
+                  class="home__site-map__item__label"
+                  :to="{ name: route.name }"
+                  >{{ route.label }}</router-link
                 >
-              </li>
-              <li class="home__site-map__item">
-                <router-link :to="{ name: 'error' }"
-                  >Measurements for Humans</router-link
-                >
-              </li>
-              <li class="home__site-map__item">
-                <router-link :to="{ name: 'error' }"
-                  >Nature Identifications</router-link
-                >
-              </li>
-              <li class="home__site-map__item">
-                <router-link :to="{ name: 'error' }">RogueSky</router-link>
-              </li>
-              <li class="home__site-map__item">
-                <router-link :to="{ name: 'error' }">TreeCount</router-link>
               </li>
             </ul>
           </div>
@@ -104,6 +88,8 @@ import Background from "@/components/Background.vue";
 import ForeverBackground from "@/components/ForeverBackground.vue";
 import LifeIcons from "@/components/LifeIcons.vue";
 
+import routeJson from "@/assets/routes.json";
+
 export default {
   name: "home",
   components: {
@@ -117,7 +103,9 @@ export default {
       personWaveFilePath: require("@/assets/people/person-wave.svg"),
       personParachuteFilePath: require("@/assets/people/person-parachute.svg"),
       personSittingFilePath: require("@/assets/people/person-sitting.svg"),
-      isImageModalActive: false
+      isImageModalActive: false,
+      routeJson: routeJson,
+      siteMapGroups: ["general", "projects"]
     };
   }
 };
@@ -179,14 +167,18 @@ export default {
 .home__site-map__header {
   font-size: 1rem;
   font-weight: bold;
+  text-transform: capitalize;
 }
 
 .home__site-map__item {
-  font-size: 1rem;
-  padding: 10px;
+  padding: 0.5rem;
 }
 
-.home__site-map__item a:hover {
+.home__site-map__item__label {
+  font-size: 1rem;
+}
+
+.home__site-map__item__label:hover {
   color: #3273dc;
 }
 
