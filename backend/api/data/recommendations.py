f682032eab9709562d126faccfeeda5425d21ef4
Camen <camenpihor@gmail.com>
2020-05-25 15:55:18 -0500

    order by label descending

--- backend/api/data/recommendations.py
+++ backend/api/data/recommendations.py
@@ -9,14 +9,14 @@ _logger = logger.setup(__name__)
 
 def get(pg_url):
     with pg_cursor(pg_url=pg_url, cursor_factory=RealDictCursor) as cursor:
-        cursor.execute("SELECT * FROM recommendations ORDER BY label;")
+        cursor.execute("SELECT * FROM recommendations ORDER BY label DESC;")
         _logger.info(cursor.statusmessage)
         return cursor.fetchall()
 
 
 def get_by_kind(kind, pg_url):
     with pg_cursor(pg_url=pg_url, cursor_factory=RealDictCursor) as cursor:
-        query = "SELECT * FROM recommendations WHERE kind = %s ORDER BY label;"
+        query = "SELECT * FROM recommendations WHERE kind = %s ORDER BY label DESC;"
         cursor.execute(query, (kind,))
         _logger.info(cursor.statusmessage)
         return cursor.fetchall()
@@ -24,7 +24,7 @@ def get_by_kind(kind, pg_url):
 
 def get_by_tag(tag, pg_url):
     with pg_cursor(pg_url=pg_url, cursor_factory=RealDictCursor) as cursor:
-        query = "SELECT * FROM recommendations WHERE %s = ANY(tags) ORDER BY label;"
+        query = "SELECT * FROM recommendations WHERE %s = ANY(tags) ORDER BY label DESC;"
         cursor.execute(query, (tag,))
         _logger.info(cursor.statusmessage)
         return cursor.fetchall()
@@ -40,7 +40,7 @@ def get_unique_tags(pg_url):
             DISTINCT(UNNEST(tags))
             FROM recommendations
         ) as uniq_tags
-        ORDER by uniq_tags
+        ORDER by uniq_tags DESC
         ;
         """
         cursor.execute(query)
@@ -51,7 +51,7 @@ def get_unique_tags(pg_url):
 
 def get_unique_kinds(pg_url):
     with pg_cursor(pg_url=pg_url) as cursor:
-        query = "SELECT DISTINCT(kind) from recommendations ORDER BY kind;"
+        query = "SELECT DISTINCT(kind) from recommendations ORDER BY kind DESC;"
         cursor.execute(query)
         _logger.info(cursor.statusmessage)
         kinds = cursor.fetchall()
