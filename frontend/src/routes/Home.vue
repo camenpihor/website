2a4f6fba00b44e100299bda314a08daecf086de2
Camen <camenpihor@gmail.com>
2020-06-05 19:22:20 -0500

    Move home page waver to the star background

--- frontend/src/routes/Home.vue
+++ frontend/src/routes/Home.vue
@@ -1,11 +1,11 @@
 <template>
   <section class="home">
     <Background class="home__image" :filePath="backgroundFilePath" />
+    <img class="person-wave" :src="personWaveFilePath" />
     <div class="home__text">
       <div class="section">
         <h1 class="title is-2 has-text-centered is-relative">
           Hai! I'm Camen :)
-          <img class="person-wave" :src="personWaveFilePath" />
         </h1>
 
         <div class="subsection">
@@ -14,13 +14,26 @@
           </h1>
           <div class="content">
             <p>
-              I'm a recently unemployed data scientist in my late 20s, who is taking some time to amble. My roaming is currently taking me to the stars, trees, birds, and the clouds, but I'm not too sure where I'll end up. I like to think that I am a good Bayesian and scientist, but I also like seeking wonder which makes me want to talk about the world in different ways.
+              I'm a recently unemployed data scientist in my late 20s, who is
+              taking some time to amble. My roaming is currently taking me to
+              the stars, trees, birds, and the clouds, but I'm not too sure
+              where I'll end up. I like to think that I am a good Bayesian and
+              scientist, but I also like seeking wonder which makes me want to
+              talk about the world in different ways.
             </p>
             <p>
-              I made this site as a collection for my amblings. Here, you'll find some thoughts of mine, some projects, and some of the things that I enjoy. I try to link to as many things that I can, in order to spread my objectively good tastes as far as I can.
+              I made this site as a collection for my amblings. Here, you'll
+              find some thoughts of mine, some projects, and some of the things
+              that I enjoy. I try to link to as many things that I can, in order
+              to spread my objectively good tastes as far as I can.
             </p>
             <p>
-              Anyways, if you've found this page, I hope that you enjoy it. If you feel like you want to, feel free to send me an email with a question! I like talking about most things for a short while, but I can talk forever about physics, statistics, astronomy, ecology, fantasy, and science fiction. I may even have a fun fact or two to share :)
+              Anyways, if you've found this page, I hope that you enjoy it. If
+              you feel like you want to, feel free to send me an email with a
+              question! I like talking about most things for a short while, but
+              I can talk forever about physics, statistics, astronomy, ecology,
+              fantasy, and science fiction. I may even have a fun fact or two to
+              share :)
             </p>
           </div>
         </div>
@@ -38,7 +51,9 @@
             <span class="is-size-5 has-text-weight-bold">Blog</span>
           </router-link>
           <p class="subtitle is-6">
-            Just a space for my thoughts on things. Probably mostly on topics that I find interesting or wondrous. And probably not much on current events.
+            Just a space for my thoughts on things. Probably mostly on topics
+            that I find interesting or wondrous. And probably not much on
+            current events.
           </p>
         </div>
         <div class="subsection">
@@ -48,7 +63,8 @@
             >
           </router-link>
           <p class="subtitle is-6">
-            Every once in a while, I try to see how much I understand a math or stats topic by trying to implement it.
+            Every once in a while, I try to see how much I understand a math or
+            stats topic by trying to implement it.
           </p>
         </div>
         <div class="subsection">
@@ -58,7 +74,11 @@
             >
           </router-link>
           <p class="subtitle is-6">
-            I used to find it really hard to not be trapped inside my own brain when walking through the natual world. So, I tried just being able to name the things I was seeing, and I actually found this incredibly enriching to my day-to-day. This is a place to gather the names I've learned and the things that make of the places I've been.
+            I used to find it really hard to not be trapped inside my own brain
+            when walking through the natual world. So, I tried just being able
+            to name the things I was seeing, and I actually found this
+            incredibly enriching to my day-to-day. This is a place to gather the
+            names I've learned and the things that make of the places I've been.
           </p>
         </div>
         <div class="subsection">
@@ -66,7 +86,11 @@
             <span class="is-size-5 has-text-weight-bold">RogueSky</span>
           </router-link>
           <p class="subtitle is-6">
-            One of the places I can constantly go to feel wonder is the sky. Albeit not incredibly surprising, it took me a long while to learn this about myself! To aid my ability to look up, I created a forecast of star visibility over the short-term, and a collection of astronomical events over the long-term.
+            One of the places I can constantly go to feel wonder is the sky.
+            Albeit not incredibly surprising, it took me a long while to learn
+            this about myself! To aid my ability to look up, I created a
+            forecast of star visibility over the short-term, and a collection of
+            astronomical events over the long-term.
           </p>
         </div>
       </div>
@@ -143,9 +167,10 @@ export default {
 
 .person-wave {
   position: absolute;
-  margin-left: 1rem;
-  top: 0.4rem;
   width: 0.8rem;
+  float: right;
+  left: 10vw;
+  top: 15vh;
 }
 
 .person-parachute {
